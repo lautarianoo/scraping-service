913783a067dc2a952bdf0c6fab2c818da984d3d0
lautarianoo <neonchick1>
2021-03-05 20:36:57 +0500

    lautariano

---
+++ scraping/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import City, NameSpecialization, Vacancy
+
+admin.site.register(City)
+admin.site.register(NameSpecialization)
+admin.site.register(Vacancy)
---
+++ scraping/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+class City(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+    class Meta():
+        verbose_name = 'City'
+        verbose_name_plural = "City's"
+    def __str__(self):
+        return self.name
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.name
+        super().save(*args, **kwargs)
+
+class NameSpecialization(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Специализация или профессия')
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta():
+        verbose_name = 'Specialization/Profession'
+        verbose_name_plural = 'Names Specializations'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.name
+        super().save(*args, **kwargs)
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    specialization = models.ForeignKey('NameSpecialization', on_delete=models.CASCADE)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
